00631f04a822347a0ed9b9f5dfc87d52d6b3acc5
afoong <afoong@calpoly.edu>
2011-03-02 23:15:28 -0800

    jaeglw

--- server.js
+++ server.js
@@ -48,9 +48,11 @@ http.createServer(function(req, res) {
 
    db = new Db('errrecorderdb', new Server(HOST, DBPORT, {}), {});
    getType(function(er){
-      sys.puts("error 1 is ");
-      console.log(er['type'] + " -> " + er['msg']);
-      sys.puts("\n");
+      if(er) {
+         sys.puts("error 1 is ");
+         console.log(er['type'] + " -> " + er['msg']);
+         sys.puts("\n");
+      }
    });
 
    //db.open(function(err, db) {
